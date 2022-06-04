aaf9d9880f00b2135ae850303ae350d38cbd008f
emmanuel <blimanuevil@gmail.com>
2022-06-04 03:03:35 +0200

    mise en forme mobile et desktop

---
+++ js/dropdown.js
@@ -0,0 +1,57 @@
+const input=document.getElementById("country");
+const countries=document.getElementById("countries");
+input.onfocus = function () {
+    countries.style.display = 'block';  
+};
+console.log(countries.options);
+for (let option of countries.options) {
+    option.onclick = function () {
+        input.value = option.value;
+        countries.style.display = 'none';
+    }
+};
+
+input.oninput = function() {
+    currentFocus = -1;
+    var text = input.value.toUpperCase();
+    for (let option of countries.options) {
+        if(option.value.toUpperCase().indexOf(text) > -1){
+            option.style.display = "block";
+        }else{
+            option.style.display = "none";
+        }
+    };
+}
+
+var currentFocus = -1;
+input.onkeydown = function(e) {
+    if(e.keyCode == 40){
+        currentFocus++
+        addActive(countries.options);
+    }
+    else if(e.keyCode == 38){
+        currentFocus--
+        addActive(countries.options);
+    }
+    else if(e.keyCode == 13){
+        e.preventDefault();
+            if (currentFocus > -1) {
+            /*and simulate a click on the "active" item:*/
+            if (countries.options) countries.options[currentFocus].click();
+            }
+    }
+}
+
+function addActive(x) {
+    if (!x) return false;
+    removeActive(x);
+    if (currentFocus >= x.length) currentFocus = 0;
+    if (currentFocus < 0) currentFocus = (x.length - 1);
+    x[currentFocus].classList.add("active");
+    console.log("active");
+}
+function removeActive(x) {
+    for (var i = 0; i < x.length; i++) {
+    x[i].classList.remove("active");
+    }
+}
